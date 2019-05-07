aaf4fb75f7a0de8d7926476b78dbafa6487d68ef
prgs1 <rodolfo@cin.ufpe.br>
2019-05-07 19:27:05 +0000

    new front on react

--- front_app/src/App.js
+++ front_app/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Container, Section } from 'rbx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Section>
+      <Container>
+        Saúde em Suas Mãos
+      </Container>
+    </Section>
+  </div>
+)
 
 export default App;
