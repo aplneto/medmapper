aa0033ceb63a0d0c10c71772b6647e0c2850e3e3
aplneto <apln2@cin.ufpe.br>
2019-05-07 12:36:53 -0300

    change database to match original open database

--- back_app/app/models/hospital.rb
+++ back_app/app/models/hospital.rb
@@ -1,2 +1,19 @@
 class Hospital < ApplicationRecord
+    self.primary_key = 'registry'
+    
+    validates :registry, presence: true, length: { is: 7 },
+    numericality: { only_integer: true }
+
+    validades :name, presence: true, length: { in: 10..100 }
+    validades :telephone, presence: true, length: { is: 10 }
+    validades :address, presence: true, length: { maximum: 50 }
+    validades :neighborhood, presence: true, length: { maximum: 30}
+
+    validades :latitude, :longitude, presence: true, length: { is: 20}
+
+    validades :region, :micro_region, presence: true, length: { is: 1},
+    numericality: { only_integer: true }
+
+    validades :nature, presence: true, length: { maximum: 15 }
+    validades :specialties, presence: true, length { maximum: 80 }
 end
--- back_app/app/models/user.rb
+++ back_app/app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+
+    validates :login, presence: true, uniqueness: true, length: { maximum:15 }
+    validates :name, presence: true, length: { maximum: 30 }
+    validates :sex, presence: true, inclusion: { in: %w(m f) }
+    validates :telephone, length: { maximum: 11 }
+    validates :address, length: { maximum: 50 }
+    validates :neighborhood, length: { maximum: 30 }
+
+    validates :adult?
+    validates :birthday, presence: true
+
+    def adult?
+        if (Time.new - Time.new birthday) / (365*24*60*60) < 18
+            errors.add(:birthday, 'é necessário ter mais de 18 anos para se
+                cadastrar')
+        end
 end
