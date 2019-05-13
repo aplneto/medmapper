4cd3109df1266148c786c111dccb03d5ac9ddb12
prgs1 <rodolfo@cin.ufpe.br>
2019-05-12 22:40:39 -0300

    commentaries table/model correcting

--- back_app/app/models/user.rb
+++ back_app/app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
     validates :sex, presence: true, inclusion: { in: %w(m f) }
     validates :telephone, length: { maximum: 11 }
     validates :email, presence: true, uniqueness: true, length: { maximum: 30 } 
+    has_many :comentaries
 end
--- back_app/db/migrate/20190513013154_create_commentaries.rb
+++ back_app/db/migrate/20190513013154_create_commentaries.rb
@@ -2,6 +2,7 @@ class CreateCommentaries < ActiveRecord::Migration[5.2]
   def change
     create_table :commentaries do |t|
       t.string :commentary, :limit => 140, null: false
+      t.references :user, foreign_key: true
       t.references :hospital, foreign_key: true
       t.timestamps
     end
