cf2e93d041b808f6eb468da25f3476f34d55cf63
Isac Silva <its@cin.ufpe.br>
2019-05-29 15:24:01 -0300

    Remoção de uniqueness

--- back_app/app/models/health_unit.rb
+++ back_app/app/models/health_unit.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 class HealthUnit < ApplicationRecord
+  validates :cnes, presence: true, numericality: true
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :address, presence: true, length: { maximum: 100 }
+  validates :neighborhood, presence: true, length: { maximum: 30 }
+  validates :phone, length: { maximum: 25 }
+  validates :latitude, :longitude, presence: true, length: { maximum: 20 }
 
-    validates :cnes, presence: true, numericality: true, uniqueness: true
-    validates :name, presence: true, length: { maximum: 100 }
-    validates :address, presence: true, length: { maximum: 100 }
-    validates :neighborhood, presence: true, length: { maximum: 30 }
-    validates :phone, length: { maximum: 25 }
-    validates :latitude, :longitude, presence: true, length: { maximum: 20 }
+  has_one :hospital
+  has_one :pharmacy
+  has_one :specialized_unit
+  has_one :basic_health_unit
 
-    has_one :hospital
-    has_one :pharmacy
-    has_one :specialized_unit
-    has_one :basic_health_unit
-
-    #polymorphic association to comments
-    has_many :comments, as: :page
-
+  # polymorphic association to comments
+  has_many :comments, as: :page
 end
