0e5a334d972d12f0ca5c99fe64083139ab484fc9
gabrielcavalcanti13 <gcm2@cin.ufpe.br>
2019-05-29 20:09:22 +0000

    polyclinics controllers

--- back_app/app/controllers/polyclinics_controller.rb
+++ back_app/app/controllers/polyclinics_controller.rb
@@ -60,6 +60,58 @@ class PolyclinicsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def basic_search
+    if params[:keywords].empty?
+      redirect_to polyclinics_path
+    else
+      @polyclinics = Polyclinic.where("specialties && :kw or 
+        treatments && :kw", kw: params[:keywords].split(' '))
+      respond_to do |format|
+        format.html { render template: "polyclinics/index.html.slim" }
+        format.json { render template: "polyclinics/index.json.jbuilder"}
+      end
+    end
+  end
+
+  def list_by_specialties
+    if params[:specialty].nil?
+      redirect_to polyclinics_path
+    else
+      @specialty = params[:specialty]
+      @polyclinics = Polyclinic.where("specialties && ARRAY[?]",
+        @specialty)
+      respond_to do |format|
+        format.html { render template: "polyclinics/specialty.html.slim" }
+        format.json { render template: "polyclinics/index.json.jbuilder"}
+      end
+    end
+  end
+
+  def list_by_treatments
+    if params[:treatments].empty?
+      redirect_to polyclinics_path
+    else
+      @polyclinic = Polyclinic.where("treatments && ?",
+        params[:treatments].split(' '))
+      respond_to do |format|
+        format.html { render template: "polyclinics/index.html.slim" }
+        format.json { render template: "polyclinics/index.json.jbuilder"}
+      end
+    end
+  end
+
+  def search_by_neighborhood
+    if params[:neighborhood].nil?
+      redirect_to polyclinics_path
+    else
+      @polyclinics = Polyclinic.where(neighborhood: params[:neighborhood])
+      respond_to do |format|
+        format.html { render template: "polyclinics/index.html.slim" }
+        format.json { render template: "polyclinics/index.json.jbuilder"}
+      end
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
