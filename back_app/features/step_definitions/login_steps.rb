0cd2a7a52a81a8ce80660702912bd4b813e10953
Isac Silva <its@cin.ufpe.br>
2019-06-03 02:25:31 -0300

    Correção na página visitada

--- back_app/features/step_definitions/login_steps.rb
+++ back_app/features/step_definitions/login_steps.rb
@@ -1,15 +1,21 @@
-Dado("que eu estou na página de login") do
-  visit('/login')
+# frozen_string_literal: true
+
+Dado('que eu estou na página de login') do
+  visit('accounts/sign_in')
 end
 
-Quando("eu preencho {string} com {string}") do |string, string2|
-  fill_in("string", :with => string2)
+Quando('eu preencho {string} com {string}') do |string, string2|
+  fill_in(string, with: string2)
 end
 
-Entao("o aplicativo deverá me redireciona para o meu perfil de usuário") do
+Quando('eu clicar no botão {string}') do |string|
+  click_button(string)
+end
+
+Então('o aplicativo deverá me redireciona para o meu perfil de usuário') do
   expect(page).to have_current_path(user_access_path(search: 'my_profile'))
 end
 
-Entao("vejo na tela:") do |string|
+Então('vejo na tela:') do |string|
   page.has_content?(string)
 end
