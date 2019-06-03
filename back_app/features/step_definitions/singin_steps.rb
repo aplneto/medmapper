58a585fc44880f65fb7a50c24a45591faed92011
Isac Silva <its@cin.ufpe.br>
2019-06-03 02:37:06 -0300

    Página visitada, variáveis não utilizadas

--- back_app/features/step_definitions/singin_steps.rb
+++ back_app/features/step_definitions/singin_steps.rb
@@ -1,44 +1,52 @@
-Dado("que eu estou na página de registro") do
-  visit('/singin')
+# frozen_string_literal: true
+
+Dado('que eu estou na página de registro') do
+  visit('accounts/sign_up')
 end
 
-Quando("eu preencho os campos {string},{string},{string},{string} com dados válidos") do |string, string2, string3, string4|
-  fill_in("E-mail", :with => 'gcm2@cin.ufpe.br')
-  fill_in("Senha", :with => 'gcm2')
-  fill_in("Confirmação de Senha", :with => 'gcm2')
-  fill_in("Nome", :with => 'Gabriel')
+Quando('eu preencho os campos {string},{string},{string},{string} com dados válidos') do |email, senha, confirmacao_senha, nome|
+  fill_in(email, with: 'gcm2@cin.ufpe.br')
+  fill_in(senha, with: 'gcm2')
+  fill_in(confirmacao_senha, with: 'gcm2')
+  fill_in(nome, with: 'Gabriel')
 end
 
-Quando("eu pressiono o botão {string}") do |string|
+Quando('eu pressiono o botão {string}') do |string|
   click_button(string)
 end
 
-Entao("sou redirecionado para o perfil usuário") do
+Quando('eu pressiono a caixa de seleção {string}') do |string|
+  find(:css, "#account_terms[value='1']").set(false)
+end
+
+Quando('eu não pressiono a caixa de seleção {string}') do |string|
+  find(:css, "#account_terms[value='1']").set(false)
+end
+
+Entao('sou redirecionado para o perfil usuário') do
   expect(page).to have_current_path(user_access_path(search: 'my_profile'))
 end
 
-Quando("eu preencho os campos {string},{string},{string} com dados válidos") do |string, string2, string3|
-  fill_in("Telefone", :with => '983727372')
-  fill_in("Sexo", :with => 'M')
-  fill_in("Nome", :with => 'Gabriel')
-  
+Quando('eu preencho os campos {string},{string},{string} com dados válidos') do |telefone, sexo, nome|
+  fill_in(telefone, with: '983727372')
+  fill_in(sexo, with: 'M')
+  fill_in(nome, with: 'Gabriel')
 end
 
-Entao("o botão {string} permanece desabilitado") do |string|
-  expect(page).to have_button('Cadastrar', disabled: true)
+Entao('o botão {string} permanece desabilitado') do |cadastrar|
+  expect(page).to have_button(cadastrar, disabled: true)
 end
 
-Quando("eu preencho os campos {string},{string},{string},{string} com senhas diferentes") do |string, string2, string3, string4|
-  fill_in("E-mail", :with => 'gcm2@cin.ufpe.br')
-  fill_in("Senha", :with => 'gcm2')
-  fill_in("Confirmação de Senha", :with => 'gdyfsuhb')
-  fill_in("Nome", :with => 'Gabriel')
+Quando('eu preencho os campos {string},{string},{string},{string} com senhas diferentes') do |email, senha, confirmacao_senha, nome|
+  fill_in(email, with: 'gcm2@cin.ufpe.br')
+  fill_in(senha, with: 'gcm2')
+  fill_in(confirmacao_senha, with: 'gdyfsuhb')
+  fill_in(nome, with: 'Gabriel')
 end
 
-Entao("uma mensagem {string} é mostrada") do |string|
+Entao('uma mensagem {string} é mostrada') do |string|
   page.has_content?(string)
 end
 
-Então("todos os campos devem ser esvaziados") do
-  
+Então('todos os campos devem ser esvaziados') do
 end
