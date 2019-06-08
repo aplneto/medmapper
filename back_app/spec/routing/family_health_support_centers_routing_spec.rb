c501319d0c59df6641e133bbfb020bf58f4b6645
Isac Silva <its@cin.ufpe.br>
2019-06-08 16:42:57 -0300

    Testes de rotas family_health_support_centers

--- back_app/spec/routing/family_health_support_centers_routing_spec.rb
+++ back_app/spec/routing/family_health_support_centers_routing_spec.rb
@@ -1,38 +1,39 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe FamilyHealthSupportCentersController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(:get => "/family_health_support_centers").to route_to("family_health_support_centers#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/centros-de-apoio').to route_to('family_health_support_centers#index')
     end
 
-    it "routes to #new" do
-      expect(:get => "/family_health_support_centers/new").to route_to("family_health_support_centers#new")
+    it 'routes to #new' do
+      expect(get: '/centros-de-apoio/new').to route_to('family_health_support_centers#new')
     end
 
-    it "routes to #show" do
-      expect(:get => "/family_health_support_centers/1").to route_to("family_health_support_centers#show", :id => "1")
+    it 'routes to #show' do
+      expect(get: '/centros-de-apoio/1').to route_to('family_health_support_centers#show', id: '1')
     end
 
-    it "routes to #edit" do
-      expect(:get => "/family_health_support_centers/1/edit").to route_to("family_health_support_centers#edit", :id => "1")
+    it 'routes to #edit' do
+      expect(get: '/centros-de-apoio/1/edit').to route_to('family_health_support_centers#edit', id: '1')
     end
 
-
-    it "routes to #create" do
-      expect(:post => "/family_health_support_centers").to route_to("family_health_support_centers#create")
+    it 'routes to #create' do
+      expect(post: '/centros-de-apoio').to route_to('family_health_support_centers#create')
     end
 
-    it "routes to #update via PUT" do
-      expect(:put => "/family_health_support_centers/1").to route_to("family_health_support_centers#update", :id => "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/centros-de-apoio/1').to route_to('family_health_support_centers#update', id: '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(:patch => "/family_health_support_centers/1").to route_to("family_health_support_centers#update", :id => "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/centros-de-apoio/1').to route_to('family_health_support_centers#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(:delete => "/family_health_support_centers/1").to route_to("family_health_support_centers#destroy", :id => "1")
+    it 'routes to #destroy' do
+      expect(delete: '/centros-de-apoio/1').to route_to('family_health_support_centers#destroy', id: '1')
     end
   end
 end
