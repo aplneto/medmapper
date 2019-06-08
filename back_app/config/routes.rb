841573a7adb2c10c550448f3ebaf7fef44b0eab3
Isac Silva <its@cin.ufpe.br>
2019-06-08 16:38:02 -0300

    Alteração na rota family_health_support_centers

--- back_app/config/routes.rb
+++ back_app/config/routes.rb
@@ -1,8 +1,9 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
-  resources :family_health_support_centers
+Rails.application.routes.draw do
+  resources :family_health_support_centers, path: 'centros-de-apoio'
   root to: 'pages#home'
-  
+
   get '/about', to: 'pages#about'
   get '/collaborators', to: 'pages#collaborators'
   get '/developers', to: 'pages#developers'
@@ -13,39 +14,39 @@ Rails.application.routes.draw do
   get '/404', to: 'errors#not_found'
   get '/422', to: 'errors#unprocessable_entity'
   get '/500', to: 'errors#internal_error'
-  
+
   resources :user_profiles, path: 'usuarios'
 
   devise_for :accounts, controllers: {
     sessions: 'accounts/sessions',
     confirmations: 'accounts/confirmations',
     passwords: 'accounts/passwords',
-    registrations: 'accounts/registrations',
+    registrations: 'accounts/registrations'
   }
-  
+
   devise_scope :accounts do
     get 'signup', to: 'accounts/registrations#new'
     get 'signin', to: 'accounts/sessions#new'
     delete 'signout', to: 'accounts/sessions#destroy'
   end
 
-  resource_names = %w'basic_health_unit diagnosis_unit emergency_unit
-  family_health_unit hospital maternity_clinic mental_health_unit
-  odontology_unit pharmacy polyclinic specialized_unit health_units'
+  resource_names = %w[basic_health_unit diagnosis_unit emergency_unit
+                      family_health_unit hospital maternity_clinic mental_health_unit
+                      odontology_unit pharmacy polyclinic specialized_unit health_units]
 
-  path_names = %w'unidades-basicas apoio-diagnostico emergencias
-  saude-da-familia hospitais maternidades saude-mental odontologia
-  farmacias policlinicas unidades-especializadas unidades'
+  path_names = %w[unidades-basicas apoio-diagnostico emergencias
+                  saude-da-familia hospitais maternidades saude-mental odontologia
+                  farmacias policlinicas unidades-especializadas unidades]
 
   resource_names.zip(path_names).map do |resource_name, path_name|
     resources resource_name.pluralize.to_sym, path: path_name do
       resources :comments, path: 'comentarios'
       collection do
-      get :basic_search, path: 'resultados'
-      get :advanced_search, path: 'pesquisar'
-      get :list_by_specialties, path: 'especialidades', as: :specialty
-      get :list_by_treatments, path: 'atendimentos', as: :treatments
-      get :search_by_neighborhood, path: 'bairro', as: :neighborhood
+        get :basic_search, path: 'resultados'
+        get :advanced_search, path: 'pesquisar'
+        get :list_by_specialties, path: 'especialidades', as: :specialty
+        get :list_by_treatments, path: 'atendimentos', as: :treatments
+        get :search_by_neighborhood, path: 'bairro', as: :neighborhood
       end
     end
   end
