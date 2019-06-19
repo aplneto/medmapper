3692be568a7b9376a5d183a72a8722dd45c0d5eb
Isac Silva <its@cin.ufpe.br>
2019-06-19 08:59:51 -0300

    Redefinição do método current_ability

--- back_app/app/controllers/application_controller.rb
+++ back_app/app/controllers/application_controller.rb
@@ -1,19 +1,24 @@
-class ApplicationController < ActionController::Base
-    protect_from_forgery
+# frozen_string_literal: true
 
-    def verify_account_confirmation
-        if current_account.confirmed_at.nil?
-            redirect_to new_account_confirmation_path,
-            notice: "Você precisa confirmar seu email para continuar"
-        end
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+  def verify_account_confirmation
+    if current_account.confirmed_at.nil?
+      redirect_to new_account_confirmation_path,
+                  notice: 'Você precisa confirmar seu email para continuar'
     end
-    
-    def assert_account_has_profile
-        unless UserProfile.exists?(account_id: current_account.id)
-            redirect_to new_user_profile_path,
-            notice: "Você precisa concluir a criação do seu perfil para 
-            continuar"
-        end
+  end
+
+  def assert_account_has_profile
+    unless UserProfile.exists?(account_id: current_account.id)
+      redirect_to new_user_profile_path,
+                  notice: "Você precisa concluir a criação do seu perfil para
+                  continuar"
     end
-    
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_account)
+  end
 end
