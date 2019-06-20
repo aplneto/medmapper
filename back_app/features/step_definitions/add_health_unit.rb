263f088b47804920f0d1463fd9789ae62c286e23
Isac Silva <34802731+isacits@users.noreply.github.com>
2019-06-19 23:56:37 -0300

    Remoção de ambiguidade

--- back_app/features/step_definitions/add_health_unit.rb
+++ back_app/features/step_definitions/add_health_unit.rb
@@ -1,19 +1,13 @@
-Dado("que eu estou na página de adicionar nova unidade de saude") do
+# frozen_string_literal: true
+
+Dado('que eu estou na página de adicionar nova unidade de saude') do
   visit('unidades/new')
 end
 
-Quando("eu preencho {string} com {string}") do |string, string2|
-  fill_in(string, with: string2)
+Quando('eu seleciono a caixa de seleção {string} com {string}') do |string, string2|
+  select(string, from: string2)
 end
 
-Quando("eu seleciono a caixa de seleção {string} com {string}") do |string, string2|
-  select('string2', :from => 'string')
-end
-
-Quando("eu clico no botão {string}") do |string|
-  click_button('string')
-end
-
-Então("sou redirecionado para página da unidade de saude") do
+Então('sou redirecionado para página da unidade de saude') do
   visit('unidades/')
 end
